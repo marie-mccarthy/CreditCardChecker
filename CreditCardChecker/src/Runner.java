6d18a5653df99b7ad6b8ee2e467b5da96dc22500
marie-mccarthy <marie.mccarthy2000@gmail.com>
2016-10-30 12:46:31 -0600

    Nearly there

--- CreditCardChecker/src/Runner.java
+++ CreditCardChecker/src/Runner.java
@@ -1,46 +1,72 @@
-
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
-import java.io.*;
 public class Runner
 	{
-		public static void main(String[] args)throws IOException
+		public static void main(String[] args) throws FileNotFoundException
 		{
+			int counter = 0;
+			long digit = 0;
+			long amountTwo = 0;
+			long amountOne = 0;
+			long total = 0;
+			long creditCardNumber = 0;
+			long totalEven = 0;
+			long totalOdd = 0;
 	      Scanner file = new Scanner( new File( "CreditCardNumbers.txt" ) );
-	//      long num = file.nextLong();
 	      long correctNum = 5424180123456789L;
-	      long evenTotal = 0;
-	      long oddTotal = 0;
-	      long [] digits = new long [16];
 	      for(int i = 0; i< 100; i++)
-	    	  {	    		  
-	    		  long digit = correctNum % 10;
-	    		  correctNum = correctNum / 10;
-	    //		  total = 0+digit;
-	    		  digits [i] =digit;
-//	    		  if(i == 2||i == 4 || i == 8 ||i == 10|| i == 12||i == 16 )
-//	    			  {
-//	    				  digit.set(digit*2)
-//	    			  }
-	    		  if(digits[i]%2!==1)
+	    	  {	    	
+	    	  creditCardNumber = file.nextLong();
+	    	  long number = 0;
+	    	  for(int place = 0; place<16 ; place++)
+	    	  {
+	    		  if(place%2==1)
+	    		  {
+	    			  digit= creditCardNumber%10*2;
+	    			  if(digit>9)
 	    			  {
-	    				  digit = digit*2;
-	    				  evenTotal = digit+evenTotal;
+	    				  // if there is more than one digit in the double
+	    				  amountOne = digit%10;
+	    				  digit=digit/10;
+	    				  amountTwo= digit;
+	    				  totalEven = totalEven+ amountOne+amountTwo;
+	    				  creditCardNumber= creditCardNumber/10;
 	    			  }
-	    		  
-//	    		  if(num.length() = 1)
-//	    			  {
-//	    				  num = num;
-//	    			  }
-//	    		  if(num.length>1)
-//	    			  {
-//	    				  
-//	    			  }
-	    	  }
-//	    		  for(long a : digits)
-//	    			  {
-//	    				  System.out.println(a+" ");
-//	    			  }
+	    			  else
+	    			  {
+	    				  //if there is not more than one digit in the double
+	    				  total = total+digit;
+	    				  creditCardNumber = creditCardNumber /10;
+	    			  }
+	    		  }
+	    		  else
+	    		  {
+	    			  total = digit
+	    			  totalAll= total+creditCardNumber/10;
+	    			  creditCardNumber= creditCardNumber/10;
+	    		  }
+//	    		  if(total%10 == 0)
+//	    		  {
+//	    			  counter++;
+//	    			  System.out.println("possible Credit card Number #"+counter);
+//	    		  }
+//	    		  else
+//	    		  {
+//	    			  System.out.println("Not a valid number");
+//	    		  }
+	    		  if(total%10 != 0)
+	    		  {
+	    			  
+	    			  System.out.println("not a valid number");
+	    		  }
+	    		  else
+	    		  {
+	    			  counter++; 
+	    			  System.out.println("This could be a valid number   "+counter);
+	    		  }
 	    	  }
 		}
+	}
 	}
